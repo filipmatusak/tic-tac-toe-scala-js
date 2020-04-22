3ac71b94025a1f9f1726a6e38e41792ba13d1b13
filipmatusak <filip.matusak@gmail.com>
2020-04-22 17:10:10 +0200

    heroku

---
+++ submit-heroku.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbt fullOptJS
+
+cd ./target/scala-2.11/classes/
+echo '{}' > composer.json
+echo '<?php include_once("home.html"); ?>' > index.php
+mv index.html home.html
+cp ../tic-tac-toe-opt.js ./js/
+git add .
+git commit -am "heroku"
+git push heroku master
+
