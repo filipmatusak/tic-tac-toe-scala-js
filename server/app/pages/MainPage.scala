ae93864f096636a222f48068e0174f364bcf6ad1
filipmatusak <filip.matusak@gmail.com>
2020-04-25 20:41:28 +0200

    heroku conf

--- server/app/pages/MainPage.scala
+++ server/app/pages/MainPage.scala
@@ -10,7 +10,9 @@ object MainPage {
       head(
         title("Tic tac toe"),
         link(rel:="stylesheet",`type`:="text/css",href:=controllers.routes.Assets.versioned("stylesheets/materialize.min.css").url),
-        link(rel:="stylesheet",`type`:="text/css",href:=controllers.routes.Assets.versioned("stylesheets/main.css").url)
+        link(rel:="stylesheet",`type`:="text/css",href:=controllers.routes.Assets.versioned("stylesheets/main.css").url),
+        meta(name:="viewport",content:="width=device-width, initial-scale=1.0"),
+        meta(charset:="UTF-8")
       ),
       body(
         div(id:="mainContainer"),
